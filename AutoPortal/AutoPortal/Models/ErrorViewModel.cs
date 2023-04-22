6a52a06904dced2e635bad9ffb490602672bd02a
vbalu01 <vhbal2001@gmail.com>
2023-04-22 17:09:23 +0200

    App Init

---
+++ AutoPortal/AutoPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
