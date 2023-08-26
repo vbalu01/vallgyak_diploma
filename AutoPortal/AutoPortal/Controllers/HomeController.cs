df9adf053a25a6597220d461c76c0143b65df276
vbalu01 <vhbal2001@gmail.com>
2023-08-26 15:19:29 +0200

    Profile update, password change (backend)

--- AutoPortal/AutoPortal/Controllers/HomeController.cs
+++ AutoPortal/AutoPortal/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using AutoPortal.Models;
 using AutoPortal.Models.AppModels;
 using AutoPortal.Models.DbModels;
+using AutoPortal.Models.RequestModels;
 using Microsoft.AspNetCore.Mvc;
 using NToastNotify;
 using System.Diagnostics;
@@ -76,6 +77,191 @@ namespace AutoPortal.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> saveUserChanges(string email, string name)
+        {
+            if (user != null)
+            {
+                if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(name))
+                {
+                    _Notification.AddErrorToastMessage("Minden mezőt ki kell tölteni!");
+                    return BadRequest("Bad data!");
+                }
+                bool change = false;
+                if(user.email != email) {
+                    if(_SQL.users.Any(u=>u.email == email) || _SQL.services.Any(s=>s.email == email) || _SQL.dealers.Any(d=>d.email == email) || _SQL.factories.Any(f=>f.email == email))
+                    {
+                        _Notification.AddInfoToastMessage("Sikertelen módosítás! Az E-mail cím már használatban van.");
+                        return BadRequest("Az email cím már használatban van");
+                    }
+                    else {
+                        change = true;
+                        user.email = email;
+                    }
+                }
+                if(user.name != name) { 
+                    change = true;
+                    user.name = name;
+                }
+
+                if (change) {
+                    _SQL.users.Update(user);
+                    _SQL.SaveChanges();
+                    _Notification.AddSuccessToastMessage("Sikeres módosítás!");
+                    return Ok("Sikeres módosítás!");
+                }
+                else {
+                    _Notification.AddInfoToastMessage("Nem történt módosítás!");
+                    return Ok("Nem történt módosítás!");
+                }
+                
+            }
+            else
+            {
+                _Notification.AddErrorToastMessage("Auth hiba!");
+                return BadRequest("Auth hiba!");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> changeUserPassword(string oldPwd, string newPwd, string newPwdRe)
+        {
+            if(user == null || loginType != eVehicleTargetTypes.USER) {
+                _Notification.AddErrorToastMessage("Auth hiba!");
+                return BadRequest("Auth hiba!");
+            }
+            if(!PasswordManager.AreEqual(oldPwd, user.password)) {
+                _Notification.AddInfoToastMessage("A régi jelszó nem megfelelő!");
+                return BadRequest("A régi jelszó nem megfelelő!");
+            }
+            if(string.IsNullOrEmpty(newPwd) || newPwd.Length < 6) {
+                _Notification.AddInfoToastMessage("Az új jelszónak legalább 6 karaktert kell tartalmaznia!");
+                return BadRequest("Az új jelszónak legalább 6 karaktert kell tartalmaznia!");
+            }
+            if(newPwd != newPwdRe) {
+                _Notification.AddInfoToastMessage("Az új jelszavak nem egyeznek!");
+                return BadRequest("Az új jelszavak nem egyeznek.");
+            }
+
+            user.password = PasswordManager.GenerateHash(newPwd);
+
+            _SQL.users.Update(user);
+            _SQL.SaveChanges();
+            _Notification.AddSuccessToastMessage("Sikeres jelszó módosítás!");
+            return Ok("Sikeres jelszó módosítás!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> saveCompanyChanges([FromBody] UpdateCompanyDataModel m)
+        {
+            if(user == null)
+            {
+                _Notification.AddErrorToastMessage("Auth hiba!");
+                return BadRequest("Auth hiba!");
+            }
+            if(loginType != eVehicleTargetTypes.SERVICE && loginType != eVehicleTargetTypes.DEALER) {
+                _Notification.AddErrorToastMessage("Hibás fiók!");
+                return BadRequest("Hibás fiók!");
+            }
+            if (!ModelState.IsValid)
+            {
+                _Notification.AddErrorToastMessage("Hiányos adatok!");
+                return BadRequest("Hiányos adatok!");
+            }
+            
+            if(user.email != m.email) {
+                if (_SQL.users.Any(u => u.email == m.email) || _SQL.services.Any(s => s.email == m.email) || _SQL.dealers.Any(d => d.email == m.email) || _SQL.factories.Any(f => f.email == m.email))
+                {
+                    _Notification.AddInfoToastMessage("Sikertelen módosítás! Az E-mail cím már használatban van.");
+                    return BadRequest("Az email cím már használatban van");
+                }
+                else
+                {
+                    user.email = m.email;
+                }
+            }
+            if(user.phone != m.phone) { 
+                user.phone = m.phone;
+            }
+            if(user.name != m.name) {
+                user.name = m.name;
+            }
+            if(user.description != m.description) { 
+                user.description = m.description;
+            }
+            if(user.country != m.country) {
+                user.county = m.country;
+            }
+            if(user.city != m.city) { 
+                user.city = m.city;
+            }
+            if(user.address != m.address) { 
+                user.address = m.address;
+            }
+            if(user.website != m.website) { 
+                user.website = m.website;
+            }
+
+            if(loginType == eVehicleTargetTypes.SERVICE) {
+                _SQL.services.Update(user);
+            }
+            else if(loginType == eVehicleTargetTypes.DEALER) {
+                _SQL.dealers.Update(user);
+            }
+            else {
+                _Notification.AddErrorToastMessage("Hibás fiók!");
+                return BadRequest("Hibás fiók!");
+            }
+            _SQL.SaveChanges();
+            _Notification.AddSuccessToastMessage("Sikeres módosítás!");
+            return Ok("Sikeres módosítás!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> changeCompanyPassword(string oldPwd, string newPwd, string newPwdRe)
+        {
+            if (user == null)
+            {
+                _Notification.AddErrorToastMessage("Auth hiba!");
+                return BadRequest("Auth hiba!");
+            }
+
+            if (!PasswordManager.AreEqual(oldPwd, user.password))
+            {
+                _Notification.AddInfoToastMessage("A régi jelszó nem megfelelő!");
+                return BadRequest("A régi jelszó nem megfelelő!");
+            }
+            if (string.IsNullOrEmpty(newPwd) || newPwd.Length < 6)
+            {
+                _Notification.AddInfoToastMessage("Az új jelszónak legalább 6 karaktert kell tartalmaznia!");
+                return BadRequest("Az új jelszónak legalább 6 karaktert kell tartalmaznia!");
+            }
+            if (newPwd != newPwdRe)
+            {
+                _Notification.AddInfoToastMessage("Az új jelszavak nem egyeznek!");
+                return BadRequest("Az új jelszavak nem egyeznek.");
+            }
+
+            user.password = PasswordManager.GenerateHash(newPwd);
+
+            if (loginType == eVehicleTargetTypes.SERVICE)
+            {
+                _SQL.services.Update(user);
+            }
+            else if(loginType == eVehicleTargetTypes.DEALER)
+            {
+                _SQL.dealers.Update(user);
+            }
+            else
+            {
+                _Notification.AddErrorToastMessage("Hibás fiók!");
+                return BadRequest("Hibás fiók!");
+            }
+            _SQL.SaveChanges();
+            _Notification.AddSuccessToastMessage("Sikeres jelszó módosítás!");
+            return Ok("Sikeres jelszó módosítás!");
         }
     }
 }
---
+++ AutoPortal/AutoPortal/Models/RequestModels/UpdateCompanyDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPortal.Models.RequestModels
+{
+    public class UpdateCompanyDataModel
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string phone { get; set; }
+        [Required]
+        public string country { get; set; }
+        [Required]
+        public string city { get; set; }
+        [Required]
+        public string address { get; set; }
+        public string website { get; set; }
+        [Required]
+        public string description { get; set; }
+
+    }
+}
