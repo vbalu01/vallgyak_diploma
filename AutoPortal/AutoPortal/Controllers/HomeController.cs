4658c9a4c29787f8e1d7061ba4320563e363cbed
vbalu01 <vhbal2001@gmail.com>
2024-01-20 18:09:42 +0100

    Doc, small modifications

--- AutoPortal/AutoPortal/Controllers/HomeController.cs
+++ AutoPortal/AutoPortal/Controllers/HomeController.cs
@@ -24,6 +24,7 @@ namespace AutoPortal.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Mail = MailSettingsModel.defaultSender;
             return View();
         }
         
